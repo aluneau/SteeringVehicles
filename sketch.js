9a19e3e2e30acb35ee84563021de9b847c66dd5d
Adrien <adriencom@msn.com>
2017-05-01 23:11:02 +0200

    modifs

--- sketch.js
+++ sketch.js
@@ -9,10 +9,10 @@ function setup() {
 	createCanvas(720, 400);
 	background(200);
 
-	for(var i=0; i < 10; i++){
+	for(var i=0; i < 5; i++){
 		vehicles.push(new Vehicle(Math.random()*width,Math.random()*height));
 	}
-	for (var i=0; i<10; i++){
+	for (var i=0; i<2000; i++){
 		food.push(createVector(Math.random()*width, Math.random()*height));
 	}
 }
@@ -22,7 +22,7 @@ function draw() {
 	mousePos = createVector(mouseX, mouseY);
 
 	for(var i = 0; i < food.length; i++){
-		ellipse(food[i].x, food[i].y, 7);
+		ellipse(food[i].x, food[i].y, 5);
 	}
 
 	for(var i=0; i<vehicles.length; i++){
@@ -30,7 +30,4 @@ function draw() {
 		vehicles[i].update();
 		vehicles[i].draw();
 	}
-	// vehicle.eat(food);
-	// vehicle.update();
-	// vehicle.draw();
 }
